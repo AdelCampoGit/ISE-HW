9cf08c6f8d37c7118e5fed453d37c1531cccda1b
MZaramalilea <153915774+MZaramalilea@users.noreply.github.com>
2023-12-15 22:46:20 +0100

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,7 +5,7 @@ int main() {
     std::cout << "\n Hello I am Alberto!";
     std::cout << "\n Hello I am Anton!";
     std::cout << "\n Hello I am Andres!";
-    std::cout << "\n Hello I am Ruben!;
-    
+    std::cout << "\n Hello I am Ruben!";
+    std::cout << "\n Hello I am Miguel!";
     return 0;
 }
